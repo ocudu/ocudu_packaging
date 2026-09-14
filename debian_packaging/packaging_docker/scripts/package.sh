c084016b66afdb6fa8bd6a0b3f61afe32345279c
Alfredo Sáez <alfredo.saez@srs.io>
2026-09-14 18:09:40 +0000

    all: update license format to use reuse
    Signed-off-by: Alfredo Sáez <alfredo.saez@srs.io>

--- debian_packaging/packaging_docker/scripts/package.sh
+++ debian_packaging/packaging_docker/scripts/package.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-#
-# Copyright 2021-2026 Software Radio Systems Limited
-#
-# By using this file, you agree to the terms and conditions set
-# forth in the LICENSE file which can be found at the top level of
-# the distribution.
-#
+
+# SPDX-FileCopyrightText: Copyright (C) 2021-2026 Software Radio Systems Limited
+# SPDX-License-Identifier: BSD-3-Clause-Open-MPI
 
 #
 # This script will package a particular release
